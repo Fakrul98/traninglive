80cc257a6c3b8be668a6ec636ef98685bd1fc820
Fakru_Hasan <fakrulhasan98@gmail.com>
2019-07-31 21:26:49 -0700

    form action design

--- index.php
+++ index.php
@@ -5,13 +5,31 @@ spl_autoload_register(function($class){
 
 $obj = new student();
 
-foreach ($obj->readAll() as $key => $value) {
-?>
-<tr>
 
-	<td><?php echo $value['name']."</br>";?></td>
-	</td> <?php echo $value['email']."</br>";?></td></tr>
+?>
+<table border="1">
+	<tr>
+		<td>Serial</td>
+		<td>Name</td>
+		<td>Email</td>
+		<td>Action</td>
+	</tr>
+
+	<?php 
+	$i = 0;
+	foreach ($obj->readAll() as $key => $value) { 
+		$i++;
+	?>
+	
+<tr>
+	<td><?php echo $i ;?></td>
+	<td><?php echo $value['name'];?></td>
+	<td><?php echo $value['email'];?></td>
+	<td><a href="">update</a>||<a href="">Delete</a></td>
+</tr> 
 <?php
 }
-
 ?>
+</table>	
+
+
